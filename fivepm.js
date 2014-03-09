d82606f08b8ded3a29b7bbe9d84de38046ccd4b5
Stuart Baker <sdb@stuartdb.com>
2014-03-10 12:03:12 +1300

    Refactor after learning more js

--- fivepm.js
+++ fivepm.js
@@ -1,54 +1,51 @@
-function draw_grid(context, color, sx, sy, w, h, s) {
+(function () {
     'use strict';
-    var x = sx,
-        y = sy;
+    var canvas = document.getElementById('fivepm'),
+        context = canvas.getContext('2d');
 
-    for (x; x < w; x = x + s) {
-        context.moveTo(x, sy);
-        context.lineTo(x, h);
+    function draw_grid(color, sx, sy, w, h, s) {
+        var x = sx,
+            y = sy;
+
+        for (x; x < w; x = x + s) {
+            context.moveTo(x, sy);
+            context.lineTo(x, h);
+        }
+
+        for (y; y < h; y = y + s) {
+            context.moveTo(sx, y);
+            context.lineTo(w, y);
+        }
+
+        context.strokeStyle = color;
+        context.stroke();
     }
 
-    for (y; y < h; y = y + s) {
-        context.moveTo(sx, y);
-        context.lineTo(w, y);
+    function draw_ui(color, x, y, w, h) {
+        context.strokeRect(x, y, w, h);
+        context.strokeStyle = color;
+        context.stroke();
     }
 
-    context.strokeStyle = color;
-    context.stroke();
-}
+    function draw_text(color, font, x, y, w, text) {
+        context.font = font;
+        context.strokeStyle = color;
+        context.fillText(text, x, y, w);
+    }
 
-function draw_ui(context, color, x, y, w, h) {
-    'use strict';
+    function draw_fivepm() {
+        var color_grid = 'rgb(220,220,220)',
+            color_font = 'rgb(0,0,0)',
+            color_ui = 'rgb(220,220,220)';
 
-    context.strokeRect(x, y, w, h);
-    context.strokeStyle = color;
-    context.stroke();
-}
 
-function draw_text(context, color, font, x, y, w, text) {
-    'use strict';
+        draw_grid(color_grid, 9.5, 9.5, 390, 290, 10);
+        draw_ui(color_ui, 9.5, 299.5, 380, 80);
 
-    context.font = font;
-    context.strokeStyle = color;
-    context.fillText(text, x, y, w);
-}
+        draw_text(color_font, "12px sans-serif", 14.5, 314.5, 370,
+                  'It is 5:00 pm. Time to go home.');
+    }
 
-function draw_fivepm() {
-    'use strict';
-    var canvas,
-        context,
-        color_grid = 'rgb(220,220,220)',
-        color_font = 'rgb(0,0,0)',
-        color_ui = 'rgb(220,220,220)';
+    return draw_fivepm();
 
-    canvas = document.getElementById('fivepm');
-    context = canvas.getContext('2d');
-
-    draw_grid(context, color_grid, 9.5, 9.5, 390, 290, 10);
-    draw_ui(context, color_ui, 9.5, 299.5, 380, 80);
-
-    draw_text(context, color_font, "12px sans-serif", 14.5, 314.5, 370,
-             'It is 5:00 pm. Time to go home.');
-}
-
-draw_fivepm();
+}());
