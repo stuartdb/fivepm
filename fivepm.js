f8e916dd4fcdf0d462767bc2d9ef0e2bf8b9ad22
Stuart Baker <sdb@stuartdb.com>
2014-03-10 14:43:06 +1300

    Add crude player movement

--- fivepm.js
+++ fivepm.js
@@ -1,24 +1,73 @@
 (function () {
     'use strict';
     var canvas = document.getElementById('fivepm'),
-        context = canvas.getContext('2d');
+        context = canvas.getContext('2d'),
 
-    function draw_grid(color, sx, sy, w, h, s) {
-        var x = sx,
-            y = sy;
+        colors = {
+            'grid' : 'rgb(220,220,220)',
+            'font' : 'rgb(0,0,0)',
+            'ui'   : 'rgb(220,220,220)'
+        },
 
-        for (x; x < w; x = x + s) {
-            context.moveTo(x, sy);
-            context.lineTo(x, h);
+        grid = {
+            'x'      : 9.5,
+            'y'      : 9.5,
+            'width'  : 390,
+            'height' : 290,
+            'cell'   : 10
+        },
+
+        player = {
+            'x'  : 10,
+            'y'  : 10,
+        },
+
+        map = '****************************';
+
+    function log_event(e) {
+        console.log(e.keyCode);
+    }
+
+
+    function handle_input(e) {
+        if (e.keyCode === 87) {
+            player.y = player.y - 1;
+        } else if (e.keyCode === 83) {
+            player.y = player.y + 1;
+        } else if (e.keyCode === 65) {
+            player.x = player.x - 1;
+        } else if (e.keyCode === 68) {
+            player.x = player.x + 1;
+        } else {
+            log_event(e);
+        }
+    }
+
+    function draw_grid() {
+        var x = grid.x,
+            y = grid.y;
+
+        for (x; x < grid.width; x = x + grid.cell) {
+            context.moveTo(x, grid.y);
+            context.lineTo(x, grid.height);
         }
 
-        for (y; y < h; y = y + s) {
-            context.moveTo(sx, y);
-            context.lineTo(w, y);
+        for (y; y < grid.height; y = y + grid.cell) {
+            context.moveTo(grid.x, y);
+            context.lineTo(grid.width, y);
         }
 
-        context.strokeStyle = color;
+        context.strokeStyle = colors.grid;
         context.stroke();
+    }
+
+    function draw_at_grid_cell(x, y, code) {
+        context.fillStyle = code;
+        context.fillRect(grid.x + (x * grid.cell),
+                 grid.y + (y * grid.cell),
+                 grid.cell,
+                 grid.cell
+                );
     }
 
     function draw_ui(color, x, y, w, h) {
@@ -34,18 +83,25 @@
     }
 
     function draw_fivepm() {
-        var color_grid = 'rgb(220,220,220)',
-            color_font = 'rgb(0,0,0)',
-            color_ui = 'rgb(220,220,220)';
+        canvas.width = canvas.width;
+        draw_grid(colors.grid, 9.5, 9.5, 390, 290, 10);
+        draw_ui(colors.ui, 9.5, 299.5, 380, 80);
 
-
-        draw_grid(color_grid, 9.5, 9.5, 390, 290, 10);
-        draw_ui(color_ui, 9.5, 299.5, 380, 80);
-
-        draw_text(color_font, "12px sans-serif", 14.5, 314.5, 370,
+        draw_text(colors.font, "12px sans-serif", 14.5, 314.5, 370,
                   'It is 5:00 pm. Time to go home.');
+
+        draw_at_grid_cell(player.x, player.y, 'rgb(0,0,0)');
+
     }
 
-    return draw_fivepm();
+    function update(e) {
+        handle_input(e);
+        draw_fivepm();
+    }
+
+    canvas.focus();
+    canvas.addEventListener('keydown', update, false);
+
+    return draw_fivepm(map);
 
 }());
